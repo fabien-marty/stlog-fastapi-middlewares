1a238963560b110839b4053c6e34cf75fbedc3da
Fabien MARTY <121722087+fabien-marty@users.noreply.github.com>
2025-06-16 15:23:23 +0200

    add ignore-hook for access-log middleware

--- tests/test_access_log.py
+++ tests/test_access_log.py
@@ -3,6 +3,7 @@ import json
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 import stlog
+from starlette.types import Scope
 
 from stlog_fastapi_middlewares.access_log import AccessLogMiddleware
 
@@ -13,8 +14,25 @@ app.add_middleware(AccessLogMiddleware, logger=logger)
 client = TestClient(app)
 
 
+def ignore_hook(scope: Scope) -> bool:
+    print(scope.get("path"))
+    return scope.get("path") == "/foo"
+
+
+app_with_ignore_hook = FastAPI()
+app_with_ignore_hook.add_middleware(
+    AccessLogMiddleware, logger=logger, ignore_hook=ignore_hook
+)
+client_with_ignore_hook = TestClient(app_with_ignore_hook)
+
+
 @app.get("/foo")
 async def foo():
+    return {"hello", "world"}
+
+
+@app_with_ignore_hook.get("/foo")
+async def foo2():
     return {"hello", "world"}
 
 
@@ -28,3 +46,9 @@ def test_access_log(log_output: io.StringIO):
     assert decoded["method"] == "GET"
     assert decoded["level"] == "INFO"
     assert decoded["message"] == "access log"
+
+
+def test_access_log_with_ignore_hook(log_output: io.StringIO):
+    response = client_with_ignore_hook.get("/foo")
+    assert response.status_code == 200
+    assert len(log_output.getvalue()) == 0
